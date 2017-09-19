f40ff12c9f949149582137622aa464567996770c
Dan Thareja <danthareja@gmail.com>
2017-09-19 00:50:50 -0700

    Implement Home Page's feed toggle spec

--- cypress/integration/conduit_spec.js
+++ cypress/integration/conduit_spec.js
@@ -2,22 +2,9 @@ describe('Conduit', function() {
 
   context('Home Page', function() {
 
-    beforeEach(function(){
-      cy.server()
-
-      cy.fixture('articles').as('articles')
-      cy.fixture('tags').as('tags')
-
-      cy.route('GET', '/api/articles*', '@articles')
-      cy.route('GET', '/api/tags', '@tags')
-
-      // Visiting our app before each test removes any state build up from
-      // previous tests. Visiting acts as if we closed a tab and opened a fresh one
-      // An empty string defers to baseUrl found in cypress.json
-      cy.visit('')
-    })
-
     it('should render a banner', function() {
+      cy.visit('')
+
       cy.get('div.home-page').find('div.banner').within(function() {
         cy.get('div.container').within(function() {
           cy.get('h1.logo-font').should('have.text', 'conduit')
@@ -26,7 +13,9 @@ describe('Conduit', function() {
       })
     })
 
-    it('should render a layout with a single row and two columns', function() {
+    it('should render a content layout with a single row and two columns', function() {
+      cy.visit('')
+
       cy.get('div.home-page').find('div.container.page').within(function() {
         cy.get('div.row').within(function() {
           cy.get('.col-md-9')
@@ -35,13 +24,34 @@ describe('Conduit', function() {
       })
     })
 
+
+    it('should render a feed toggle in the left column', function() {
+      cy.visit('')
+
+      cy.get('div.home-page').find('div.col-md-9').within(function() {
+        cy.get('div.feed-toggle').within(function() {
+          cy.get('ul').within(function() {
+            cy.get('li.nav-item')
+              .find('a.nav-link')
+              .contains('Global Feed')
+              .should('have.class', 'active')
+          })
+        })
+      })
+    })
+
     it('should render a feed of articles in the left column', function() {
+      cy.server()
+      cy.fixture('articles').as('articles')
+      cy.route('GET', '/api/articles*', '@articles').as('getArticles')
+      cy.visit('')
+
       cy.get('div.home-page').find('div.col-md-9').within(function() {
         cy.get('div.article-preview')
           .should('have.length', this.articles.articlesCount)
-          .each(function($el, i) {
+          .each(function($article, i) {
             const article = this.articles.articles[i]
-            cy.wrap($el).within(function() {
+            cy.wrap($article).within(function() {
               cy.get('div.article-meta').within(function() {
                 cy.get('a').within(function() {
                   cy.get('img').should('have.attr', 'src', article.author.image)
@@ -63,6 +73,16 @@ describe('Conduit', function() {
                 cy.get('h1').should('have.text', article.title)
                 cy.get('p').should('have.text', article.description)
                 cy.get('span').should('have.text', 'Read more...')
+                cy.get('ul.tag-list').within(function () {
+                  cy.get('li')
+                    .each(function($tag, j) {
+                      cy.wrap($tag)
+                        .should('have.class', 'tag-pill')
+                        .should('have.class', 'tag-default')
+                        .should('have.class', 'tag-outline')
+                        .should('have.text', article.tagList[j])
+                    })
+                })
               })
             })
           })
@@ -70,6 +90,12 @@ describe('Conduit', function() {
     })
 
     it('should sort the feed by most recently created', function () {
+      cy.server()
+      cy.fixture('articles').as('articles')
+      cy.route('GET', '/api/articles*', '@articles').as('getArticles')
+      cy.visit('')
+      cy.wait('@getArticles')
+
       cy.get('div.home-page').find('div.article-preview span.date')
         .should(function($dates) {
           // TODO: Move this into a Chai plugin for better error messages
@@ -84,17 +110,13 @@ describe('Conduit', function() {
         })
     })
 
-    it('should render a feed toggle with a "Global Feed" tab', function() {
-      cy.get('div.home-page').find('div.feed-toggle').within(function() {
-        cy.get('ul').within(function() {
-          cy.get('li.nav-item')
-            .find('a.nav-link.active')
-            .should('have.text', 'Global Feed')
-        })
-      })
-    })
-
     it('should render a tag list in the right column', function() {
+      cy.server()
+      cy.fixture('tags').as('tags')
+      cy.route('GET', '/api/tags', '@tags').as('getTags')
+      cy.visit('')
+      cy.wait('@getTags')
+
       cy.get('.home-page').find('.col-md-3').within(function() {
         cy.get('div.sidebar').within(function() {
           cy.get('p').should('have.text', 'Popular Tags')
@@ -108,6 +130,52 @@ describe('Conduit', function() {
       })
     })
 
-  })
+    it('should render a tag feed when a tag in the list is clicked', function() {
+      cy.visit('')
 
+      cy.get('div.home-page').find('a.tag-pill').first().then(function(tag) {
+        const text = Cypress.$(tag).text()
+
+        // clicking a tag should
+        cy.wrap(tag).click()
+
+        // deactivate the the 'Global Feed' tab on the feed toggle
+        cy.get('div.home-page')
+          .find('div.feed-toggle')
+          .contains('a.nav-link', 'Global Feed')
+          .should('not.have.class', 'active')
+
+        // and, activate a new tab on the feed toggle
+        cy.get('div.home-page')
+          .find('div.feed-toggle')
+          .contains('a.nav-link', text)
+          .should('have.class', 'active')
+
+        // and, filter the feed
+        cy.get('div.home-page')
+          .find('div.article-preview')
+          .each(function($article, i) {
+            cy.wrap($article).contains('li.tag-default.tag-pill.tag-outline', text)
+          })
+
+        // clicking back to the 'Global Feed' tab should
+        cy.get('div.home-page')
+          .find('div.feed-toggle')
+          .contains('a.nav-link', 'Global Feed')
+          .click()
+
+        // remove the tag's tab from the feed toggle
+        cy.get('div.home-page')
+          .find('div.feed-toggle')
+          .contains('a.nav-link', text)
+          .should('not.exist')
+
+        // and, activate the 'Global Feed' tab on the feed toggle
+        cy.get('div.home-page')
+          .find('div.feed-toggle')
+          .contains('a.nav-link', 'Global Feed')
+          .should('have.class', 'active')
+      })
+    })
+  })
 })
