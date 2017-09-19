8572034e27a49fd2a28f767ff16425dea8207c9c
Dan Thareja <danthareja@gmail.com>
2017-09-18 23:17:17 -0700

    Improve home page tests with more explicit selectors

--- cypress/integration/conduit_spec.js
+++ cypress/integration/conduit_spec.js
@@ -18,7 +18,7 @@ describe('Conduit', function() {
     })
 
     it('should render a banner', function() {
-      cy.get('div.banner').within(function() {
+      cy.get('div.home-page').find('div.banner').within(function() {
         cy.get('div.container').within(function() {
           cy.get('h1.logo-font').should('have.text', 'conduit')
           cy.get('p').should('have.text', 'A place to share your knowledge.')
@@ -26,37 +26,51 @@ describe('Conduit', function() {
       })
     })
 
-    it('should render a feed of articles', function() {
-      cy.get('div.article-preview').should('have.length', this.articles.articlesCount)
-      cy.get('div.article-preview').each(function($el, i) {
-        const article = this.articles.articles[i]
-        cy.wrap($el).within(function() {
-          cy.get('div.article-meta').within(function() {
-            cy.get('a').within(function() {
-              cy.get('img').should('have.attr', 'src', article.author.image)
-            })
-            cy.get('div.info').within(function() {
-              cy.get('a.author').should('have.text', article.author.username)
-              cy.get('span.date').should('have.text', Cypress.moment(article.createdAt).format('ddd MMM DD YYYY'))
-            })
-            // TODO: Fix implementation so we can use more explict 'have.text' matcher
-            cy.get('button.btn').should('contain', article.favoritesCount)
-            cy.get('button.btn').within(function() {
-              cy.get('i.ion-heart')
-            })
-          })
-          cy.get('a.preview-link').within(function() {
-            cy.get('h1').should('have.text', article.title)
-            cy.get('p').should('have.text', article.description)
-            cy.get('span').should('have.text', 'Read more...')
-          })
+    it('should render a layout with a single row and two columns', function() {
+      cy.get('div.home-page').find('div.container.page').within(function() {
+        cy.get('div.row').within(function() {
+          cy.get('.col-md-9')
+          cy.get('.col-md-3')
         })
       })
     })
 
-    it('should sort feed by most recently created', function () {
-      cy.get('div.article-preview')
-        .find('span.date')
+    it('should render a feed of articles in the left column', function() {
+      cy.get('div.home-page').find('div.col-md-9').within(function() {
+        cy.get('div.article-preview')
+          .should('have.length', this.articles.articlesCount)
+          .each(function($el, i) {
+            const article = this.articles.articles[i]
+            cy.wrap($el).within(function() {
+              cy.get('div.article-meta').within(function() {
+                cy.get('a').within(function() {
+                  cy.get('img').should('have.attr', 'src', article.author.image)
+                })
+                cy.get('div.info').within(function() {
+                  cy.get('a.author').should('have.text', article.author.username)
+                  cy.get('span.date').should(
+                    'have.text',
+                    Cypress.moment(article.createdAt).format('ddd MMM DD YYYY')
+                  )
+                })
+                // TODO: Fix implementation so we can use more explict 'have.text' matcher
+                cy.get('button.btn').should('contain', article.favoritesCount)
+                cy.get('button.btn').within(function() {
+                  cy.get('i.ion-heart')
+                })
+              })
+              cy.get('a.preview-link').within(function() {
+                cy.get('h1').should('have.text', article.title)
+                cy.get('p').should('have.text', article.description)
+                cy.get('span').should('have.text', 'Read more...')
+              })
+            })
+          })
+      })
+    })
+
+    it('should sort the feed by most recently created', function () {
+      cy.get('div.home-page').find('div.article-preview span.date')
         .should(function($dates) {
           // TODO: Move this into a Chai plugin for better error messages
           const dates = $dates
@@ -71,7 +85,7 @@ describe('Conduit', function() {
     })
 
     it('should render a feed toggle with a "Global Feed" tab', function() {
-      cy.get('div.feed-toggle').within(function() {
+      cy.get('div.home-page').find('div.feed-toggle').within(function() {
         cy.get('ul').within(function() {
           cy.get('li.nav-item')
             .find('a.nav-link.active')
@@ -80,13 +94,15 @@ describe('Conduit', function() {
       })
     })
 
-    it('should render a tag list', function() {
-      cy.get('div.sidebar').within(function() {
-        cy.get('p').should('have.text', 'Popular Tags')
-        cy.get('div.tag-list').within(function() {
-          cy.get('a.tag-pill').each(function($el, i) {
-            const tag = this.tags.tags[i]
-            cy.wrap($el).should('have.text', tag)
+    it('should render a tag list in the right column', function() {
+      cy.get('.home-page').find('.col-md-3').within(function() {
+        cy.get('div.sidebar').within(function() {
+          cy.get('p').should('have.text', 'Popular Tags')
+          cy.get('div.tag-list').within(function() {
+            cy.get('a.tag-pill').each(function($el, i) {
+              const tag = this.tags.tags[i]
+              cy.wrap($el).should('have.text', tag)
+            })
           })
         })
       })
